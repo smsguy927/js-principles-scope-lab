259f6c9c840ff35fca520bc1be9e6e5847a439ba
smsguy927 <smsguy927@gmail.com>
2021-05-18 15:14:00 -0500

    commit tree c5a963ead804e4af793999eae58209d25b562e92

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "sally"
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(name) {
+  window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+function changeLeastFavoriteCustomer(name) {
+  leastFavoriteCustomer = name;
+}
